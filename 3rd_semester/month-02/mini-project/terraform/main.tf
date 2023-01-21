d1a96ef4438f850a9f1dfbd928e7efbadc764a40
philemonnwanne <philemonnwanne@gmail.com>
2023-01-21 13:00:29 +0100

    added locals block and fixed host-inventory output 🚏

--- 3rd_semester/month-02/mini-project/terraform/main.tf
+++ 3rd_semester/month-02/mini-project/terraform/main.tf
@@ -1,5 +1,4 @@
-/* This file defines the terraform block, which Terraform uses to configures itself. This block specifies this Terraform configuration must use the aws provider that is within the v4.49.0 minor release. It also requires that you use a Terraform version greater than v1.1.0.
-*/
+# This file defines the terraform block, which Terraform uses to configures itself. This block specifies this Terraform configuration must use the aws provider that is within the v4.49.0 minor release. It also requires that you use a Terraform version greater than [v1.1.0.]
 
 terraform {
   required_providers {
@@ -15,38 +14,60 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-/* this block configures the AWS provider */
+# this block configures the AWS provider
 provider "aws" {
-  region = var.aws_region
+  region = local.region
 }
 
+# define common tags to be assigned to all VPC resources
+locals {
+  region = var.aws_region
+  tags = {
+    Owner = "Philemon Nwanne"
+    Track = "Cloud/DevOps"
+  }
+}
+
+# import the vpc module
 module "vpc" {
   source = "./modules/vpc"
   vpc_name = "mini-vpc"
+  tags = local.tags
 }
 
+# import the security module
 module "security" {
   source = "./modules/security"
   vpc_id = module.vpc.mini_vpc_id
 }
 
+# import the ec2 module
 module "ec2" {
   source = "./modules/ec2"
-  subnet_id = module.vpc.mini_vpc_subnet_id
-  vpc_security_group_ids = [module.security.webserver_secgrp_id]
+  subnet_id = module.vpc.mini_vpc_subnet_id[0]
+  vpc_security_group_ids = [module.security.webserver_security_group_id]
 }
 
+# import the alb module
 module "alb" {
   source = "./modules/alb"
+  vpc_id = module.vpc.mini_vpc_id
+  subnets = module.vpc.mini_vpc_subnet_id[*]
+  security_groups = [module.security.alb_security_group_id]
+  target_01 = module.ec2.webserver_id[0]
+  target_02 = module.ec2.webserver_id[1]
+  target_03 = module.ec2.webserver_id[2]
 }
 
+# import the route53 module
 module "route53" {
   source = "./modules/route53"
 }
 
+# create inventory file for use by ansible
 resource "local_file" "host_inventory" {
-  content = "module.ec2.webserver_public_ip"
-  filename = "../hosts"
+  content = " ${module.ec2.webserver_public_ip[0]}\n ${module.ec2.webserver_public_ip[1]}\n ${module.ec2.webserver_public_ip[2]} "
+  filename = "../ansible/host-inventory"
 }
 
 # provisioner "local-exec" {
