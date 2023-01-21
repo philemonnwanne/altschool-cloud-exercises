d711b217fa6f5f866d56af94f235b3dc2c1e8a59
philemonnwanne <philemonnwanne@gmail.com>
2023-01-21 13:07:39 +0100

    moved the ssl variable to the alb modules input variable file 🧬

--- 3rd_semester/month-02/mini-project/terraform/variables.tf
+++ 3rd_semester/month-02/mini-project/terraform/variables.tf
@@ -3,9 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "ssl_cert" {
-  description = "the id of the instance to be deployed"
-  type        = string
-  default     = "arn:aws:acm:us-east-1:183066416469:certificate/ed9b93c3-253c-4f46-b2e2-370a7734fdb2"
-}
