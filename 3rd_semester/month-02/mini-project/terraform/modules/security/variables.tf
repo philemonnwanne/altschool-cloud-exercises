4fab931619accbc9ae939d9d680fa04ad698c2ba
philemonnwanne <philemonnwanne@gmail.com>
2023-01-21 14:26:07 +0100

    update all changes made to security module

--- 3rd_semester/month-02/mini-project/terraform/modules/security/variables.tf
+++ 3rd_semester/month-02/mini-project/terraform/modules/security/variables.tf
@@ -1,24 +1,28 @@
-variable "security_group_name" {
-  description = "security group default name"
+variable "webserver_security_group_name" {
+  description = "webserver security group name"
   type        = string
   default     = "webserver-security-group"
 }
 
-variable "vpc_id" {
-  description = "VPC default id"
-  type        = string
-}
-
-variable "security_group_ingress_cidr" {
+variable "webserver_security_group_ingress_cidr" {
   description = "security group inbound CIDR block"
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
-variable "security_group_ingress_rules" {
+variable "webserver_security_group_ingress_rules" {
   description = "security group inbound rules"
   type        = list(string)
-  default     = ["https-443-tcp"]
+  default     = [
+    "http-80-tcp",
+    "https-443-tcp",
+    "ssh-tcp"
+    ]
+}
+
+variable "vpc_id" {
+  description = "VPC default id"
+  type        = string
 }
 
 variable "security_group_egress_cidr" {
@@ -31,4 +35,25 @@ variable "security_group_egress_rules" {
   description = "security group outbound rules"
   type        = list(string)
   default     = ["all-all"]
+}
+
+variable "alb_security_group_name" {
+  description = "application load balancer security group name"
+  type        = string
+  default     = "alb-security-group"
+}
+
+variable "alb_security_group_ingress_cidr" {
+  description = "security group inbound CIDR block"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "alb_security_group_ingress_rules" {
+  description = "security group inbound rules"
+  type        = list(any)
+  default     = [
+    "http-80-tcp",
+    "https-443-tcp"
+    ]
 }
