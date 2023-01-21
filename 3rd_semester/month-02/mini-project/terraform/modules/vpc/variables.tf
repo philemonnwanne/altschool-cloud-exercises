03d836ae5593448a521498ecd680a9f4c6ded18e
philemonnwanne <philemonnwanne@gmail.com>
2023-01-21 21:44:09 +0100

    added third availability zone

--- 3rd_semester/month-02/mini-project/terraform/modules/vpc/variables.tf
+++ 3rd_semester/month-02/mini-project/terraform/modules/vpc/variables.tf
@@ -13,31 +13,31 @@ variable "vpc_cidr" {
 variable "vpc_azs" {
   description = "availability zones for VPC"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "vpc_public_subnets" {
   description = "public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.0.0/20", "10.0.16.0/20"]
+  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
 }
 
 variable "vpc_public_subnets_names" {
   description = "private subnet names for VPC"
   type        = list(string)
-  default     = ["mini-subnet-public1-us-east-1a", "mini-subnet-public2-us-east-1b"]
+  default     = ["mini-subnet-public1-us-east-1a", "mini-subnet-public2-us-east-1b","mini-subnet-public2-us-east-1c"]
 }
 
 variable "vpc_private_subnets" {
   description = "private subnets for VPC"
   type        = list(string)
-  default     = ["10.0.128.0/20", "10.0.144.0/20"]
+  default     = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20"]
 }
 
 variable "vpc_private_subnets_names" {
   description = "private subnet names for VPC"
   type        = list(string)
-  default     = ["mini-subnet-private1-us-east-1a", "mini-subnet-private2-us-east-1b"]
+  default     = ["mini-subnet-private1-us-east-1a", "mini-subnet-private2-us-east-1b", "mini-subnet-private2-us-east-1c"]
 }
 
 variable "tags" {
