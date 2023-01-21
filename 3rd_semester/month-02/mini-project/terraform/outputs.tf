8a0d7005fab4060125a7fd7268b1b231a0b8ac12
philemonnwanne <philemonnwanne@gmail.com>
2023-01-21 13:04:46 +0100

    renamed all occurances of secgrp to security_group 👮🏾‍♂️

--- 3rd_semester/month-02/mini-project/terraform/outputs.tf
+++ 3rd_semester/month-02/mini-project/terraform/outputs.tf
@@ -13,21 +13,26 @@ output "final_vpc_subnet_id" {
     value = module.vpc.mini_vpc_subnet_id
 }
 
-output "final_vpc_secgrp_id" {
+output "final_vpc_security_group_id" {
      description = "project VPC default security group ID"
-    value = module.vpc.mini_vpc_secgrp_id
+    value = module.vpc.mini_vpc_security_group_id
 }
 
-output "final_webserver_secgrp_id" {
+output "final_webserver_security_group_id" {
     description = "security group controlling traffic access to the webserver instances"
-    value = module.security.webserver_secgrp_id
+    value = module.security.webserver_security_group_id
+}
+
+output "final_alb_security_group_id" {
+    description = "security group controlling traffic access to the application load balancer"
+    value = module.security.alb_security_group_id
 }
 
 output "final_instance_public_ip" {
     description = "public IP addresses of EC2 webserver instances"
-    value = module.ec2.webserver_public_ip
+    value = module.ec2[*].webserver_public_ip
 }
 
-# output "final_number-of-instances-created" {
-#     value = length(module.ec2.webserver_count)
-# }
+output "number-of-instances-created" {
+    value = length(module.ec2.webserver_id)
+}
